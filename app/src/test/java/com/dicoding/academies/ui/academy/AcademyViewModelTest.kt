d44db992ff849bce5288ff9564722722cbb25450
Milky-way <79302158+khairul3@users.noreply.github.com>
2021-04-28 14:56:29 +0800

    academy-using-viewModel-testing

--- app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
+++ app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
@@ -3,10 +3,19 @@ package com.dicoding.academies.ui.academy
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class AcademyViewModelTest {
+    private lateinit var viewModel: AcademyViewModel
+    @Before
+    fun setUp() {
+        viewModel = AcademyViewModel()
+    }
 
     @Test
     fun getCourses() {
+        val courseEntities = viewModel.getCourses()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
     }
 }
