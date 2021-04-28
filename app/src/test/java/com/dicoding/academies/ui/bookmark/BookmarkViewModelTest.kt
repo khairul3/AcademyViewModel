30f8ec086833471863bda2bbff3a659fb9f999b2
Milky-way <79302158+khairul3@users.noreply.github.com>
2021-04-28 14:49:13 +0800

    academy-using-viewModel-testing

---
+++ app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.dicoding.academies.ui.bookmark
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class BookmarkViewModelTest {
+
+    private lateinit var viewModel: BookmarkViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = BookmarkViewModel()
+    }
+
+    @Test
+    fun getBookmark() {
+        val courseEntities = viewModel.getBookmarks()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
+}
