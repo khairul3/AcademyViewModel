1feab4ee4d05f4978b885b6282834e969c4798c8
Milky-way <79302158+khairul3@users.noreply.github.com>
2021-05-06 22:18:38 +0800

    Adding unit testing and instrumental testing for repository

---
+++ app/src/test/java/com/dicoding/academies/data/AcademyRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.dicoding.academies.data
+
+import com.dicoding.academies.data.source.remote.RemoteDataSource
+import com.dicoding.academies.data.source.remote.response.ContentResponse
+import com.dicoding.academies.data.source.remote.response.CourseResponse
+import com.dicoding.academies.data.source.remote.response.ModuleResponse
+import com.dicoding.academies.utils.DataDummy
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
+class AcademyRepositoryTest {
+
+}
